3b8afa984850d3df8502c4f2ccde8fe8d2d3ad8d
A1198266 <AS261394@wipro.com>
2017-03-30 13:58:07 +0530

    Initial commit for a sample angular app

---
+++ app/app.js
@@ -0,0 +1,2 @@
+var app = angular.module('myApp',[]);
+console.log('Angular.module created successfully');
---
+++ app/js/controllers/MainController.js
@@ -0,0 +1,4 @@
+app.controller('MainController',function($rootScope,$scope){
+	console.log('MainController Created');
+	$scope.test = "Success";
+});
